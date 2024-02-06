3d27b1947ac209c3523f7752d5fdcdb8e410630c
lindsben <li589635@ucf.edu>
2024-02-05 22:04:50 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,8 @@
+let quotes = [
+  "The only thing we have to fear is fear itself. -Franklin D. Roosevelt",
+  "Be yourself; everyone else is already taken. -Oscar Wilde",
+  "It is during our darkest moments that we must focus to see the light. -Aristotle",
+];
+
+// Get an integer between 0 and 2
+console.log(quotes[Math.floor(Math.random() * 3)]);
